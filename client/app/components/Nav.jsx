8e63ee562fba67e564d29dcdde845895b4589d61
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-10-18 22:54:26 +0100

    'change'

--- client/app/components/Nav.jsx
+++ client/app/components/Nav.jsx
@@ -1,20 +1,19 @@
 "use client";
-import React , {useState} from "react";
+import React , {useEffect, useState , useRef} from "react";
 import { FaHome } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
 const Nav = () => {
-  const [open , isOpen] = useState(false)
-  const openMenu = () => {
+  const [open , setOpen] = useState(false);
+  const menu = useRef();
+  useEffect(() => {
      if(open){
-        
-       
+      menu.current.className = menu.current.className.replace('translate-x-full','translate-0')
      }else{
-
-
+      menu.current.className = menu.current.className.replace('translate-0','translate-x-full')
      }
-  }
+  },[open])
   return (
     <div>
       <div className="flex justify-between p-4 items-center sm:w-10/12 w-11/12 mx-auto ">
@@ -22,28 +21,30 @@ const Nav = () => {
           Trendify
         </div>
         <div className="hidden sm:flex items-center">
-          <FaHome className="mx-4  primary_color text-xl sm:text-2xl" />
-          <FaShoppingCart className="mx-4  primary_color text-xl sm:text-2xl" />
+          <FaHome className="mx-4  primary_color text-xl sm:text-2xl duration-200 ease-in-out hover:scale-110 cursor-pointer" />
+          <FaShoppingCart className="mx-4  primary_color text-xl sm:text-2xl duration-200 ease-in-out hover:scale-110  cursor-pointer" />
           <button className="mx-4 text-white primary_color_bg px-4 py-1  rounded">
             Log in
           </button>
         </div>
-        <div className=" sm:hidden ">
-          <FaBars className="text-2xl cursor-pointer primary_color" />
+        <div className="sm:hidden ">
+          <FaBars className="text-2xl cursor-pointer primary_color" onClick={() => setOpen(prev => !prev)}/>
         </div>
       </div>
-      {/* <div className="flex flex-col items-end bg-slate-500 h-full w-1/3 absolute top-0 right-0 ">
-        <AiOutlineClose className="text-2xl mt-3 mr-2 cursor-pointer " />
-        <div className="flex absolute left-0 top-12 flex-col gap-4 ">
-          <FaHome className="mx-4  primary_color text-xl sm:text-2xl" />
-          <FaShoppingCart className="mx-4  primary_color text-xl sm:text-2xl" />
+
+      <div ref={menu} className=" sm:hidden flex translate-x-full  duration-200 ease-in-out flex-col items-end  z-50 h-full bg-green-100 absolute top-0 w-2/5 right-0 ">
+        <AiOutlineClose className="text-2xl mt-4 mr-4 cursor-pointer duration-200 ease-in-out hover:scale-110 " onClick={() => setOpen(prev => !prev)}/>
+        <div className="flex absolute left-0 top-16 flex-col gap-5  ">
+          <FaHome className="mx-4 primary_color text-xl sm:text-2xl cursor-pointer duration-200 ease-in-out hover:scale-110" />
+          <FaShoppingCart className="mx-4 primary_color text-xl sm:text-2xl cursor-pointer duration-200 ease-in-out hover:scale-110" />
           <button className="mx-4 text-white primary_color_bg px-4 py-1 rounded">
             Log in
           </button>
-        </div> */}
-      {/* </div> */}
+        </div> 
+      </div>
     </div>
   );
 };
 
 export default Nav;
+
