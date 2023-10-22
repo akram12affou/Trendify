e415ab12ddb04fd7dc7946599c4b8d8d5c78b036
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-10-23 00:56:06 +0100

    'change'

--- client/app/shoppingCart/page.tsx
+++ client/app/shoppingCart/page.tsx
@@ -1,7 +1,9 @@
+'use client'
 import React from "react";
-
+import {FaPlusCircle} from 'react-icons/fa'
+import {FaMinusCircle} from 'react-icons/fa'
 function page() {
-  console.log({
+  const products = [{
     category: "men's clothing",
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
@@ -10,33 +12,65 @@ function page() {
     price: 109.95,
     rating: { rate: 3.9, count: 120 },
     title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  });
+  }]
+ 
 
   return (
-    <div>
-      <div>
-        <h3>my cart</h3>
-      </div>
-      <div>
-        <h3>price details</h3>
+    <div className="flex flex-col lg:flex-row lg:w-10/12 w-11/12  mx-auto p-4 gap-4 items-start">
+      <div className="w-full shadow_product p-3 lg:w-3/4">
+        <h3 className="font-semibold  mb-8 lg:text-2xl text-xl uppercase secondary_color racking-wide brand_selection after_underline">my cart</h3>
         <div>
+          {products.map(product => {
+            return(
+              <div className="flex items-center">
+               <div className="flex flex-col justify-center gap-2 w-1/2">
+                <img src={product.image} className="mx-auto w-1/2" />
+               <div className="flex justify-center gap-2">
+                <FaMinusCircle className='cursor-pointer  sm:text-2xl text-xl'/>
+                <input className="border border-black outline-none w-1/2 "/>
+                <FaPlusCircle className='cursor-pointer sm:text-2xl text-xl'/>
+               </div>
+
+                
+               </div >
+               <div className='w-1/2 flex flex-col gap-3 items-start'>
+                <h2 className="lg:text-xl text-lg font-semibold tracking-wider">{product.title}</h2>
+                <div className="relative">
+                  <span className="font-semibold" >{product.price}£</span>
+                  <span className="absolute top-3.5 line-through text-sm ">{product.price + 50}£ </span>
+                </div>
+                 <button className='bg-red-500 text-white font-bold px-2 py-1 text_button tracking-wide rounded-sm lg:text-lg text-base '>Remove</button>
+
+
+              </div>
+              </div>
+          )})}
+        </div>
+      </div>
+      <div className="w-full lg:w-1/4 p-6 flex flex-col items-center shadow_product">
+        <h3 className="font-semibold  mb-8  uppercase secondary_color racking-wide brand_selection after_underline lg:text-2xl text-xl items-center">price details</h3>
+        <div  className="flex gap-12">
+          <div className="flex flex-col gap-3">
           <span>Price (1)</span>
           <span>Discounts</span>
           <span>Delivery charges</span>
         </div>
-        <div>
+        <div className="flex flex-col gap-3">
           <span>219.9</span>
-          <span>10</span>
-          <span>Free</span>
+          <span className="font-semibold text-green-600">10</span>
+          <span className="font-semibold text-green-600">Free</span>
         </div>
-        <hr />
-        <div>
+        </div>
+        
+        <div className="primary_color_bg h-1 rounded-lg w-1/4 lg:w-1/2 my-4 mx-auto  "/>
+        <div className="  flex flex-col ">
           <span>Total amount
-            <span>652 £</span>
+          &nbsp;&nbsp; <span className="font-semibold">652.00£</span>
           </span>
-          <span>you will save 10$ on this order</span>
-          <button>Place order</button>
-          <span>Login to place order</span>
+          <br />
+          <span className="text-green-500 font-semibold">you will save 10£ on this order</span>
+          <button className="primary_color_bg w-1/2 text-white font-bold px-1 py-1.5 text_button tracking-wider rounded-sm  mt-8 opacity-90 border-2 border-sky-400 lg:text-base text-sm">Place order</button>
+          <span className="text-red-500 font-semibold mt-2 ">Login to place order</span>
         </div>
       </div>
     </div>
